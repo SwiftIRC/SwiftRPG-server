c7a464488e05062dd6baa43769338570ae8772a3
Ryan William O'Hara <ryan@ryanwohara.com>
2023-11-18 19:13:39 -0700

    feat: Map generation

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,7 @@ mod map;
 use map::generate_map;
 
 fn main() {
-    generate_map();
+    const CURRENT_SEED: u32 = 1000;
+
+    generate_map(CURRENT_SEED);
 }
--- src/map.rs
+++ src/map.rs
@@ -1,6 +1,9 @@
+extern crate image;
 extern crate noise;
 
+use ::core::ops::Sub;
 use noise::{utils::*, *};
+use num_traits::MulAdd;
 
 struct NoiseTemplate {
     name: String,
@@ -11,8 +14,53 @@ struct NoiseTemplate {
 }
 
 pub struct Noise {
+    #[allow(dead_code)]
     noise: Fbm<Perlin>,
     name: String,
+}
+
+pub fn generate_map(seed: u32) {
+    let types = get_map_types();
+    let mut noises: Vec<Noise> = Vec::new();
+
+    let mut index: u32 = 0;
+
+    types.iter().for_each(|t| {
+        let noise = generate_noise_fn(seed + index, t);
+
+        noises.push(Noise {
+            noise: noise.clone(),
+            name: t.name.to_owned(),
+        });
+
+        imagerender_mapbuilder_raw(&noise, &format!("{}.png", noises[index as usize].name));
+
+        index += 1;
+    });
+
+    imagerender_mapbuilder(
+        &build(16, 16, 0.0, 0.1, 0.0, 0.1, true, seed),
+        "merged-small.png",
+    );
+    imagerender_mapbuilder(
+        &build(10000, 10000, 0.0, 10.0, 0.0, 10.0, true, seed),
+        "merged.png",
+    );
+}
+
+fn generate_noise_fn(seed: u32, t: &NoiseTemplate) -> Fbm<Perlin> {
+    Fbm::<Perlin>::new(seed)
+        .set_frequency(t.frequency)
+        .set_persistence(t.persistence)
+        .set_lacunarity(t.lacunarity)
+        .set_octaves(t.octaves)
+}
+
+fn generate_noises(seed: u32) -> Vec<Fbm<Perlin>> {
+    get_map_types()
+        .iter()
+        .map(|t| generate_noise_fn(seed, t))
+        .collect()
 }
 
 fn get_map_types() -> Vec<NoiseTemplate> {
@@ -41,136 +89,180 @@ fn get_map_types() -> Vec<NoiseTemplate> {
     ]
 }
 
-pub fn generate_map() -> Vec<Noise> {
-    const CURRENT_SEED: u32 = 10000;
-
-    let types = get_map_types();
-    let mut noises = Vec::new();
-
-    types.iter().for_each(|t| {
-        let i = types
-            .iter()
-            .position(|x| x.name == t.name)
-            .unwrap_or_default();
-
-        noises.push(Noise {
-            noise: Fbm::<Perlin>::new(CURRENT_SEED + i as u32)
-                .set_frequency(t.frequency)
-                .set_persistence(t.persistence)
-                .set_lacunarity(t.lacunarity)
-                .set_octaves(t.octaves),
-            name: t.name.to_owned(),
-        });
-
-        println!(
-            "{}: {}",
-            noises[i].name,
-            noises[i].noise.get([100.5, 100.5])
-        );
-
-        imagerender_mapbuilder(&noises[i].noise, &format!("{}.png", noises[i].name));
-    });
-
-    print!("{:?}: ", get_point([100.5, 100.5], "10000"));
-
-    noises
-}
-
-fn imagerender_mapbuilder(noise_map: &noise::Fbm<noise::Perlin>, filename: &str) {
+fn imagerender_mapbuilder_raw(noise_map: &noise::Fbm<noise::Perlin>, filename: &str) {
     ImageRenderer::new()
-        .render(&mapbuilder(&noise_map))
+        .render(&mapbuilder_raw(&noise_map))
         .write_to_file(filename);
 }
 
-fn mapbuilder(noise_map: &noise::Fbm<noise::Perlin>) -> NoiseMap {
+fn mapbuilder_raw(noise_map: &noise::Fbm<noise::Perlin>) -> NoiseMap {
     PlaneMapBuilder::<&noise::Cache<&noise::Fbm<noise::Perlin>>, 2>::new(&Cache::new(noise_map))
         .set_size(16, 16)
-        .set_x_bounds(-1.0, 1.0)
-        .set_y_bounds(-1.0, 1.0)
+        .set_is_seamless(true)
+        .set_x_bounds(0.0, 1.0)
+        .set_y_bounds(0.0, 1.0)
         .build()
 }
 
-fn get_point(point: [f64; 2], seed: &str) -> Vec<f64> {
-    let types = get_map_types();
-
-    let mut points = Vec::new();
-
-    types.iter().for_each(|t| {
-        let i = types
-            .iter()
-            .position(|x| x.name == t.name)
-            .unwrap_or_default();
-
-        let noise = Fbm::<Perlin>::new(seed.parse::<u32>().unwrap() + i as u32)
-            .set_frequency(t.frequency)
-            .set_persistence(t.persistence)
-            .set_lacunarity(t.lacunarity)
-            .set_octaves(t.octaves);
-
-        points.push(noise.get(point));
-
-        println!("{}: {}", t.name, noise.get(point));
-    });
-
-    points
+fn imagerender_mapbuilder(noise_map: &NoiseMap, filename: &str) {
+    ImageRenderer::new()
+        // .set_gradient(ColorGradient::new().build_terrain_gradient())
+        .set_gradient(build_terrain_gradient())
+        .render(noise_map)
+        .write_to_file(filename);
 }
 
-fn get_biome(point: [f64; 2], seed: &str) -> String {
-    let points = get_point(point, seed);
+fn build_terrain_gradient() -> ColorGradient {
+    ColorGradient::new()
+        .clear_gradient()
+        .add_gradient_point(-0.9, [0, 0, 0, 255]) // deep ocean
+        .add_gradient_point(-0.7, [0, 0, 255, 255]) // ocean
+        .add_gradient_point(-0.5, [194, 178, 128, 255]) // sand
+        .add_gradient_point(-0.25, [100, 100, 100, 255]) // tundra
+        .add_gradient_point(0.0, [214, 198, 148, 255]) // desert
+        .add_gradient_point(0.2, [5, 154, 23, 255]) // grassland
+        .add_gradient_point(0.4, [0, 100, 0, 255]) // forest
+        .add_gradient_point(0.6, [55, 128, 55, 255]) // rainforest
+        .add_gradient_point(0.8, [180, 180, 180, 255]) // mountain
+        .add_gradient_point(0.9, [255, 255, 255, 255]) // snow
+}
 
+pub fn get_points(point: [f64; 2], noises: &Vec<Fbm<Perlin>>) -> Vec<f64> {
+    let types = get_map_types();
+
+    let point = [point[0] * 10.0, point[1] * 10.0];
+
+    (0..types.len())
+        .map(|i| get_point(point, &noises[i]))
+        .collect()
+}
+
+pub fn get_point(point: [f64; 2], noise: &Fbm<Perlin>) -> f64 {
+    let point = [point[0], point[1]];
+
+    noise.get(point)
+}
+
+pub fn get_biome_as_f64(biome: String) -> f64 {
+    match biome.as_str() {
+        "deep_ocean" => -1.0,
+        "ocean" => -0.7,
+        "sand" => -0.5,
+        "tundra" => 0.25,
+        "desert" => 0.0,
+        "grassland" => 0.2,
+        "forest" => 0.4,
+        "rainforest" => 0.6,
+        "mountain" => 0.8,
+        "snow" => 1.0,
+        _ => 0.0,
+    }
+}
+
+pub fn get_biome(points: Vec<f64>) -> String {
     let height = points[0];
     let temperature = points[1];
     let humidity = points[2];
 
-    let mut biome = String::new();
+    let biome: String;
 
-    if height < 0.0 {
+    if height <= -0.5 {
+        biome = "deep_ocean".to_string();
+    } else if height <= -0.1 {
         biome = "ocean".to_string();
-    } else if height < 0.1 {
-        biome = "beach".to_string();
-    } else if height > 0.8 {
-        biome = "snow".to_string();
-    } else if height > 0.6 {
-        biome = "tundra".to_string();
-    } else if height > 0.1 {
-        if temperature < 0.25 {
-            biome = "bare".to_string();
-        } else if temperature < 0.6 {
-            biome = "taiga".to_string();
-        } else {
-            biome = "shrubland".to_string();
-        }
-    } else {
-        if temperature < 0.25 {
-            if humidity < 0.33 {
-                biome = "scorched".to_string();
-            } else if humidity < 0.66 {
-                biome = "temperate desert".to_string();
-            } else {
+    } else if height <= 0.0 {
+        biome = "sand".to_string();
+    } else if height <= 0.5 {
+        if temperature <= 0.0 {
+            biome = "tundra".to_string();
+        } else if temperature >= 0.7 {
+            if humidity <= 0.0 {
                 biome = "desert".to_string();
-            }
-        } else if temperature < 0.6 {
-            if humidity < 0.16 {
-                biome = "tundra".to_string();
-            } else if humidity < 0.50 {
+            } else if humidity <= 0.2 {
                 biome = "grassland".to_string();
-            } else if humidity < 0.83 {
-                biome = "temperate deciduous forest".to_string();
+            } else if humidity <= 0.4 {
+                biome = "forest".to_string();
             } else {
-                biome = "temperate rainforest".to_string();
+                biome = "rainforest".to_string();
             }
+        } else if humidity <= 0.33 {
+            biome = "grassland".to_string();
+        } else if humidity >= 0.66 {
+            biome = "forest".to_string();
+        } else if height <= 0.2 {
+            biome = "grassland".to_string();
         } else {
-            if humidity < 0.16 {
-                biome = "shrubland".to_string();
-            } else if humidity < 0.33 {
-                biome = "taiga".to_string();
-            } else if humidity < 0.66 {
-                biome = "temperate rainforest".to_string();
-            } else {
-                biome = "tropical rainforest".to_string();
-            }
+            biome = "forest".to_string();
         }
+    } else if height <= 0.8 {
+        biome = "mountain".to_string();
+    } else {
+        biome = "snow".to_string();
     }
 
     biome
+}
+
+fn build(
+    width: usize,
+    height: usize,
+    x1: f64,
+    x2: f64,
+    y1: f64,
+    y2: f64,
+    is_seamless: bool,
+    seed: u32,
+) -> NoiseMap {
+    let mut result_map = NoiseMap::new(width, height);
+
+    let noises = generate_noises(seed);
+    let noise = noises.first().unwrap();
+
+    let x_bounds = (x1, x2);
+    let y_bounds = (y1, y2);
+
+    let x_extent = x_bounds.1 - x_bounds.0;
+    let y_extent = y_bounds.1 - y_bounds.0;
+
+    let x_step = x_extent / width as f64;
+    let y_step = y_extent / height as f64;
+
+    for y in 0..height {
+        let current_y = y_bounds.0 + y_step * y as f64;
+
+        for x in 0..width {
+            let current_x = x_bounds.0 + x_step * x as f64;
+
+            let points = get_points([current_x, current_y], &noises);
+
+            let final_value = if is_seamless {
+                let sw_value = get_point([current_x, current_y], &noise);
+                let se_value = get_point([current_x + x_extent, current_y], &noise);
+                let nw_value = get_point([current_x, current_y + y_extent], &noise);
+                let ne_value = get_point([current_x + x_extent, current_y + y_extent], &noise);
+
+                let x_blend = 1.0 - ((current_x - x_bounds.0) / x_extent);
+                let y_blend = 1.0 - ((current_y - y_bounds.0) / y_extent);
+
+                let y0 = linear(sw_value, se_value, x_blend);
+                let y1 = linear(nw_value, ne_value, x_blend);
+
+                linear(y0, y1, y_blend)
+            } else {
+                points[0]
+            };
+
+            result_map[(x, y)] =
+                get_biome_as_f64(get_biome(vec![final_value, points[1], points[2]]));
+        }
+    }
+
+    result_map
+}
+
+fn linear<T>(a: T, b: T, x: T) -> T
+where
+    T: MulAdd<Output = T> + Sub<Output = T> + Copy,
+{
+    x.mul_add(b - a, a)
 }
