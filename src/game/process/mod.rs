b1f7e69741cc74e9ec1926ac070db1ebcb3f4e07
Ryan William O'Hara <ryan@ryanwohara.com>
2023-12-13 14:37:36 -0700

    Revert "feat: Learning basic queue functionality"
    This reverts commit 2a9ade7971695b086b79c46b6d03cc01bd8f6faf.
    
    We don't need RabbitMQ, just MySQL, using row locking.

--- src/game/process/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod fruits;
-pub mod herbs;
-pub mod players;
-pub mod rocks;
-pub mod trees;
--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,9 @@ mod game;
 mod map;
 mod web;
 
-use game::publish_task_with_timestamp;
-use lapin::Connection;
-use lapin::ConnectionProperties;
 use map::data::Chunk;
 use map::fns::generate_map;
 use map::fns::generate_noises;
-use rocket::tokio;
 use std::thread;
 use web::state::RocketState;
 
@@ -43,29 +39,6 @@ async fn main() {
 
     let mut next_timestamp = 0;
     let mut next_tick = chrono::Local::now().timestamp();
-
-    let conn = match Connection::connect(
-        "amqp://reinze:reinzepassword@localhost:5672/Reinze",
-        ConnectionProperties::default(),
-    )
-    .await
-    {
-        Ok(conn) => conn,
-        Err(e) => panic!("Error connecting to RabbitMQ: {:?}", e),
-    };
-
-    let channel = match conn.create_channel().await {
-        Ok(channel) => channel,
-        Err(e) => panic!("Error creating channel: {:?}", e),
-    };
-
-    match publish_task_with_timestamp(&channel, "foo!", (next_tick + 1) as u64).await {
-        Ok(_) => println!("Published task"),
-        Err(e) => println!("Error publishing task: {:?}", e),
-    };
-
-    channel.close(0, "").await.unwrap();
-
     thread::spawn(move || loop {
         let now = chrono::Local::now();
         let tick = now.timestamp();
@@ -77,9 +50,7 @@ async fn main() {
                 let timestamp = now.format("%D %T").to_string();
                 println!("{}", timestamp);
 
-                let rt = tokio::runtime::Runtime::new().unwrap();
-
-                rt.block_on(async { game::tick(&conn).await });
+                thread::spawn(|| game::tick());
 
                 next_tick = tick + 60 - (next_tick % 60);
             }
